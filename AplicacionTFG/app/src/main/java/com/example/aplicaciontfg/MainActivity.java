711249377f008ece051d25d59354831683f67a79
Iuliusgh <iuliusgh@ucm.es>
2023-02-27 11:39:35 +0100

    Añadida funcionalidad de camara
    Cuidado que esto lo he metido en otra rama, en main lo ponemos cuando ya funcione perfecto

--- AplicacionTFG/app/src/main/java/com/example/aplicaciontfg/MainActivity.java
+++ AplicacionTFG/app/src/main/java/com/example/aplicaciontfg/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.aplicaciontfg;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Intent intent = new Intent(this,CameraActivity.class);
+        Button botonCamara = (Button) findViewById(R.id.botonCamara);
+        botonCamara.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intent);
+            }
+        });
+
     }
 }
